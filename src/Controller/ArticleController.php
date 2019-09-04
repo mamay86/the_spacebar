922724f63096fdb4b43dabc16c7edd23bddf286d
roma_dev <roma@pay-per-calls.com>
2019-09-04 14:16:42 +0300

    Stellar Development with Symfony 4. Chapter 06

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -4,10 +4,11 @@
 namespace App\Controller;
 
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class ArticleController
+class ArticleController extends AbstractController
 {
     /**
      * @Route("/")
@@ -22,9 +23,15 @@ class ArticleController
      */
     public function show($slug)
     {
-        return new Response(sprintf(
-            'Future page to show the article %s',
-            $slug
-        ));
+        $comments = [
+            'Comment1',
+            'Comment2',
+            'Comment3',
+        ];
+
+        return $this->render('article/show.html.twig', [
+            'title' => ucwords(str_replace('-', ' ', $slug)),
+            'comments' => $comments,
+        ]);
     }
 }
