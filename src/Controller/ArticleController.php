70453bf2c28d0553f5b5946f3bfe3efc94c5ac50
roma_dev <roma@pay-per-calls.com>
2019-09-04 14:23:49 +0300

    Stellar Development with Symfony 4. Chapter 07

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -29,6 +29,8 @@ class ArticleController extends AbstractController
             'Comment3',
         ];
 
+        dump($slug, $this);
+
         return $this->render('article/show.html.twig', [
             'title' => ucwords(str_replace('-', ' ', $slug)),
             'comments' => $comments,
