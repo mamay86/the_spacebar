b304a0c5001a57dfe0cda27342798a0a67283d3d
roma_dev <roma@pay-per-calls.com>
2019-09-09 23:56:02 +0300

    Symfony Security: Beautiful Authentication, Powerful Authorization. Chapter 21

--- src/Controller/ArticleAdminController.php
+++ src/Controller/ArticleAdminController.php
@@ -12,7 +12,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @IsGranted("ROLE_ADMIN")
+ * @IsGranted("ROLE_ADMIN_ARTICLE")
  */
 class ArticleAdminController extends AbstractController
 {
