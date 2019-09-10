c65e15f28f9bcbc4d9d23d2c9b32260236095ada
roma_dev <roma@pay-per-calls.com>
2019-09-10 17:50:56 +0300

    Symfony Security: Beautiful Authentication, Powerful Authorization. Chapter 28

--- src/Security/ApiTokenAuthenticator.php
+++ src/Security/ApiTokenAuthenticator.php
@@ -3,9 +3,11 @@
 namespace App\Security;
 
 use App\Repository\ApiTokenRepository;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Core\Exception\CustomUserMessageAuthenticationException;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Core\User\UserProviderInterface;
 use Symfony\Component\Security\Guard\AbstractGuardAuthenticator;
@@ -41,7 +43,11 @@ class ApiTokenAuthenticator extends AbstractGuardAuthenticator
         ]);
 
         if (!$token) {
-            return;
+            throw new CustomUserMessageAuthenticationException('Invalid API Token');
+        }
+
+        if ($token->isExpired()) {
+            throw new CustomUserMessageAuthenticationException('Token Expired');
         }
 
         return $token->getUser();
@@ -49,26 +55,28 @@ class ApiTokenAuthenticator extends AbstractGuardAuthenticator
 
     public function checkCredentials($credentials, UserInterface $user)
     {
-        dd('checking credentials');
+        return true;
     }
 
     public function onAuthenticationFailure(Request $request, AuthenticationException $exception)
     {
-        // todo
+        return new JsonResponse([
+            'message' => $exception->getMessageKey()
+        ], 401);
     }
 
     public function onAuthenticationSuccess(Request $request, TokenInterface $token, $providerKey)
     {
-        // todo
+        // allow the request to continue
     }
 
     public function start(Request $request, AuthenticationException $authException = null)
     {
-        // todo
+        throw new \Exception('Not used: entry_point from other authentication is used');
     }
 
     public function supportsRememberMe()
     {
-        // todo
+        return false;
     }
 }
