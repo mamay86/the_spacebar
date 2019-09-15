5b496b939bc08943e0baf91653c2060bc9a71e64
Roman Nagriy <roman.nagriy@gmail.com>
2019-09-15 21:35:21 +0300

    Symfony 4 Forms: Build, Render & Conquer! Chapter 07

--- src/Form/ArticleFormClass.php
+++ src/Form/ArticleFormClass.php
@@ -5,6 +5,8 @@ namespace App\Form;
 
 
 use App\Entity\Article;
+use App\Entity\User;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
@@ -24,6 +26,13 @@ class ArticleFormClass extends AbstractType
             ->add('content')
             ->add('publishedAt', null, [
                 'widget' => 'single_text'
+            ])
+            ->add('author', EntityType::class, [
+                'class' => User::class,
+                'choice_label' => function(User $user) {
+                    return sprintf('(%d) %s', $user->getId(), $user->getEmail());
+                },
+                'placeholder' => 'Choose an author',
             ])
         ;
     }
