280939edecb0e1cd6e8d3c3015f443c44e8c6b2e
Roman Nagriy <roman.nagriy@gmail.com>
2019-09-15 16:37:47 +0300

    Symfony 4 Forms: Build, Render & Conquer! Chapter 02

--- src/Controller/ArticleAdminController.php
+++ src/Controller/ArticleAdminController.php
@@ -5,9 +5,11 @@ namespace App\Controller;
 
 
 use App\Entity\Article;
+use App\Repository\ArticleRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Form\ArticleFormClass;
@@ -18,9 +20,23 @@ class ArticleAdminController extends AbstractController
      * @Route("/admin/article/new", name="admin_article_new")
      * @IsGranted("ROLE_ADMIN_ARTICLE")
      */
-    public function new(EntityManagerInterface $em)
+    public function new(EntityManagerInterface $em, Request $request)
     {
         $form = $this->createForm(ArticleFormClass::class);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $data = $form->getData();
+            $article = new Article();
+            $article->setTitle($data['title']);
+            $article->setContent($data['content']);
+            $article->setAuthor($this->getUser());
+
+            $em->persist($article);
+            $em->flush();
+
+            return $this->redirectToRoute('app_homepage');
+        }
 
         return $this->render('article_admin/new.html.twig', [
             'articleForm' => $form->createView()
@@ -36,5 +52,16 @@ class ArticleAdminController extends AbstractController
 //        $this->denyAccessUnlessGranted('MANAGE', $article);
 
         dd($article);
+    }
+
+    /**
+     * @Route("/admin/article")
+     */
+    public function list(ArticleRepository $articleRepo)
+    {
+        $articles = $articleRepo->findAll();
+        return $this->render('article_admin/list.html.twig', [
+            'articles' => $articles,
+        ]);
     }
 }
