ef68d32a00796237a8c14016e5e7d4fcf7d1ffbc
Roman Nagriy <roman.nagriy@gmail.com>
2019-09-15 21:13:14 +0300

    Symfony 4 Forms: Build, Render & Conquer! Chapter 05

--- src/Form/ArticleFormClass.php
+++ src/Form/ArticleFormClass.php
@@ -6,6 +6,8 @@ namespace App\Form;
 
 use App\Entity\Article;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\FormInterface;
 use Symfony\Component\Form\FormView;
@@ -16,7 +18,9 @@ class ArticleFormClass extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('title')
+            ->add('title', TextType::class, [
+                'help' => 'Choose something catchy',
+            ])
             ->add('content')
         ;
     }
