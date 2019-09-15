74fb98ca8d3ea3c7342a0ed0138611f51c372db2
Roman Nagriy <roman.nagriy@gmail.com>
2019-09-15 16:47:56 +0300

    Symfony 4 Forms: Build, Render & Conquer! Chapter 03

--- src/Controller/ArticleAdminController.php
+++ src/Controller/ArticleAdminController.php
@@ -35,7 +35,9 @@ class ArticleAdminController extends AbstractController
             $em->persist($article);
             $em->flush();
 
-            return $this->redirectToRoute('app_homepage');
+            $this->addFlash('success', 'Article Created! Knowledge is power!');
+
+            return $this->redirectToRoute('admin_article_list');
         }
 
         return $this->render('article_admin/new.html.twig', [
@@ -55,7 +57,7 @@ class ArticleAdminController extends AbstractController
     }
 
     /**
-     * @Route("/admin/article")
+     * @Route("/admin/article", name="admin_article_list")
      */
     public function list(ArticleRepository $articleRepo)
     {
